69307aba8bbd59affa53506894df1db012a1f11d
B3LIOTT <55959873+B3LIOTT@users.noreply.github.com>
2024-07-04 20:23:39 +0200

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -9,20 +9,16 @@ def plot_graph(pattern: str):
     :param pattern: Pattern to plot
     """
     
-    graph1 = nx.Graph()
-    graph2 = nx.Graph()
+    graph = nx.Graph()
     
     # Add nodes
     i = 1
     for pos in NODES.values():
-        # Schema graph
-        graph1.add_node(i, pos=(pos[1], -pos[0]))
-
         # Schema graph with the solution order
         if pattern.__contains__(str(i)):
-            graph2.add_node(pattern.index(str(i))+1, pos=(pos[1], -pos[0]))
+            graph.add_node(pattern.index(str(i))+1, pos=(pos[1], -pos[0]))
         else:
-            graph2.add_node(-1, pos=(pos[1], -pos[0]))
+            graph.add_node(-1, pos=(pos[1], -pos[0]))
         i += 1
     
     # Add edges
@@ -30,17 +26,11 @@ def plot_graph(pattern: str):
     for i in range(len(pattern) - 1):
         colors[int(pattern[i])-1] = 'green'
         colors[int(pattern[i + 1])-1] = 'green'
-        graph1.add_edge(int(pattern[i]), int(pattern[i + 1]))
-        graph2.add_edge(pattern.index(pattern[i])+1, pattern.index(pattern[i + 1])+1)
+        graph.add_edge(pattern.index(pattern[i])+1, pattern.index(pattern[i + 1])+1)
 
 
 
     plt.figure()
-    nx.draw(graph1, nx.get_node_attributes(graph1, 'pos'), node_size=1000, node_color=colors, with_labels=True)
-    plt.title("Pattern: " + pattern)
-    plt.show()
-
-    plt.figure()
-    nx.draw(graph2, nx.get_node_attributes(graph2, 'pos'), node_size=1000, node_color=colors, with_labels=True)
+    nx.draw(graph, nx.get_node_attributes(graph, 'pos'), node_size=1000, node_color=colors, with_labels=True)
     plt.title("Pattern: " + pattern)
     plt.show()
